244a9fcc3c5f846d5276a389bc3949a6eaeb5bcf
timely-rain <lolic_sd@hotmail.com>
2017-09-19 22:48:37 +0800

    支持可变参数

--- timely-services/timely-services-base/src/main/java/site/timely/services/base/repository/JpaRepository.java
+++ timely-services/timely-services-base/src/main/java/site/timely/services/base/repository/JpaRepository.java
@@ -30,68 +30,66 @@ public interface JpaRepository<T, ID extends Serializable>
      * @param specification JPA Condition 查询规格
      * @return 对象是否存在
      */
-    default boolean exists(T model, ConditionSpecification<T> specification) {
+    default boolean exists(T model, ConditionSpecification<T>... specification) {
         return findAll(model, specification).size() > 0;
     }
 
     /**
      * 查询单个实体
      *
-     * @param model         实体数据
-     * @param specification JPA Condition 查询规格
+     * @param model          实体数据
+     * @param specifications JPA Condition 查询规格
      * @return Entity
      * @see ConditionSpecification
      * @see JpaSpecificationExecutor
      */
-    default T findOne(T model, ConditionSpecification<T> specification) {
+    default T findOne(T model, ConditionSpecification<T>... specifications) {
         return this.findOne(
-                JpaConditionUtils.specification(model, specification));
+                JpaConditionUtils.specification(model, specifications));
     }
 
     /**
      * 查询多个实体
      *
-     * @param model         实体数据
-     * @param specification JPA Condition 查询规格
+     * @param model          实体数据
+     * @param specifications JPA Condition 查询规格
      * @return List<Entity>
      * @see ConditionSpecification
      * @see JpaSpecificationExecutor
      */
-    default List<T> findAll(T model, ConditionSpecification<T> specification) {
+    default List<T> findAll(T model, ConditionSpecification<T>... specifications) {
         return this.findAll(
-                JpaConditionUtils.specification(model, specification));
+                JpaConditionUtils.specification(model, specifications));
     }
 
     /**
      * 查询多个实体
      *
-     * @param model         实体数据
-     * @param specification JPA Condition 查询规格
-     * @param sort          排序
+     * @param model          实体数据
+     * @param specifications JPA Condition 查询规格
+     * @param sort           排序
      * @return List<Entity>
      * @see ConditionSpecification
      * @see JpaSpecificationExecutor
      */
-    default List<T> findAll(T model, ConditionSpecification<T> specification,
-                            Sort sort) {
+    default List<T> findAll(T model, Sort sort, ConditionSpecification<T>... specifications) {
         return this.findAll(
-                JpaConditionUtils.specification(model, specification), sort);
+                JpaConditionUtils.specification(model, specifications), sort);
     }
 
     /**
      * 查询多个实体
      *
-     * @param model         实体数据
-     * @param specification JPA Condition 查询规格
-     * @param pageable      分页
+     * @param model          实体数据
+     * @param specifications JPA Condition 查询规格
+     * @param pageable       分页
      * @return List<Entity>
      * @see ConditionSpecification
      * @see JpaSpecificationExecutor
      */
-    default Page<T> findAll(T model, ConditionSpecification<T> specification,
-                            Pageable pageable) {
+    default Page<T> findAll(T model, Pageable pageable, ConditionSpecification<T>... specifications) {
         return this.findAll(
-                JpaConditionUtils.specification(model, specification), pageable);
+                JpaConditionUtils.specification(model, specifications), pageable);
     }
 
     /* ConditionSpecification Queries End */
