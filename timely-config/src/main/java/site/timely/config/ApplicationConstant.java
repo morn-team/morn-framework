1466e63974b351a80de607430d0af096d9d0de47
timely-rain <lolic_sd@hotmail.com>
2018-05-09 17:16:58 +0800

    新增应用常量

---
+++ timely-config/src/main/java/site/timely/config/ApplicationConstant.java
@@ -0,0 +1,19 @@
+package site.timely.config;
+
+/**
+ * 应用常量
+ *
+ * @author timely-rain
+ * @verion 1.0.0, 2018/5/9
+ * @since 1.0-SNAPSHOT
+ */
+public interface ApplicationConstant {
+    /**
+     * 国际化属性文件名
+     */
+    String MESSAGE_PROPERTIES = "timely.messages";
+    /**
+     * Json属性文件名
+     */
+    String JSON_PROPERTIES = "timely.json";
+}
