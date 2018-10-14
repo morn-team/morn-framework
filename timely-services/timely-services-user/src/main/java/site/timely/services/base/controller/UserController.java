6cf5b1289ba3f6757ecee1a2802629b6b0e36ef5
timely-rain <lolic_sd@hotmail.com>
2018-10-14 12:21:41 +0800

    使用RepositoryRestResource

--- timely-services/timely-services-user/src/main/java/site/timely/services/base/controller/UserController.java
+++ timely-services/timely-services-user/src/main/java/site/timely/services/base/controller/UserController.java
@@ -1,16 +1,57 @@
 package site.timely.services.base.controller;
 
+import org.springframework.context.MessageSource;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.timely.message.MessageHolder;
+import site.timely.services.base.domain.User;
+import site.timely.services.base.service.UserService;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
 
 /**
- * site.timely.services.base.controller
+ * 用户控制器
  *
  * @author timely-rain
- * @verion 1.0.0, 2017/9/28
- * @since 1.8
+ * @version 1.0.0, 2017/9/28
+ * @since 1.0-SNAPSHOT
  */
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
+    @Resource
+    private UserService service;
+
+    @Resource
+    private MessageHolder messageHolder;
+
+    @Resource
+    private MessageSource messageSource;
+
+    @PostMapping
+    public Object add(@Validated User user) {
+//        if (result.hasErrors()) {
+//            List<FieldError> fieldErrors = result.getFieldErrors();
+//            List<String> messages = new ArrayList<>();
+//            for (FieldError fieldError : fieldErrors) {
+//                String message = messageSource.getMessage(fieldError, messageHolder.currentLocale());
+//                String fieldPath = fieldError.getObjectName() + "." + fieldError.getField();
+//                String fieldName = messageSource.getMessage(fieldPath, null, fieldPath, messageHolder.currentLocale());
+//                messages.add(fieldName + message);
+//            }
+//            String s = StringUtils.collectionToCommaDelimitedString(messages);
+//            return Rests.error().message(s);
+//        }
+        return new HashMap<>();
+    }
+
+    @PostMapping("datatable")
+    public List<User> datatable() {
+        List<User> all = service.findAll();
+        return all;
+    }
 }
--- timely-services/timely-services-user/src/main/java/site/timely/services/base/repository/PrivilegeRepository.java
+++ timely-services/timely-services-user/src/main/java/site/timely/services/base/repository/PrivilegeRepository.java
@@ -1,27 +1,24 @@
 package site.timely.services.base.repository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import site.timely.services.base.domain.Privilege;
-
-import java.util.List;
 
 /**
  * 权限库
  *
  * @author timely-rain
- * @verion 1.0.0, 2017/9/18
+ * @version 1.0.0, 2017/9/18
  * @since 1.0-SNAPSHOT
  */
-@NoRepositoryBean
+//@NoRepositoryBean
+@RepositoryRestResource
 public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
-    /**
-     * 所有权限码
-     *
-     * @param username 用户名
-     * @return 权限码集合
-     */
-    @Query("select code from Privilege where roles.users.username = :username")
-    List<String> findCodes(@Param("username") String username);
+//    /**
+//     * 所有权限码
+//     *
+//     * @param username 用户名
+//     * @return 权限码集合
+//     */
+//    @Query("select code from Privilege where roles.users.username = :username")
+//    List<String> findCodes(@Param("username") String username);
 }
