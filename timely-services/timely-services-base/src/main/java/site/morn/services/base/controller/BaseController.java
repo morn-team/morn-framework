af22109388a94193b451b3a70cd39fd88df7e18a
timely-rain <lolic_sd@hotmail.com>
2018-10-23 21:32:01 +0800

    移除MessageHolder和HttpException相关，计划采用Morn系列

--- timely-services/timely-services-base/src/main/java/site/morn/services/base/controller/BaseController.java
+++ timely-services/timely-services-base/src/main/java/site/morn/services/base/controller/BaseController.java
@@ -1,9 +1,5 @@
 package site.morn.services.base.controller;
 
-import javax.annotation.Resource;
-import site.morn.exception.HttpExceptionProvider;
-import site.morn.http.HttpResultProvider;
-
 /**
  * 基础控制器
  *
@@ -13,14 +9,4 @@ import site.morn.http.HttpResultProvider;
  */
 public class BaseController {
 
-  /**
-   * 网络请求结果提供者
-   */
-  @Resource
-  protected HttpResultProvider provider;
-  /**
-   * 网络异常提供者
-   */
-  @Resource
-  protected HttpExceptionProvider exceptionProvider;
 }
