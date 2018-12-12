441662fdeff7cf8c6fdb1fe08dd40ae5c0be1286
timely-rain <lolic_sd@hotmail.com>
2018-12-13 00:11:03 +0800

    配置FastJson的响应头

--- timely-config/src/main/java/site/morn/message/FastJsonConfiguration.java
+++ timely-config/src/main/java/site/morn/message/FastJsonConfiguration.java
@@ -3,10 +3,13 @@ package site.morn.message;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import site.morn.config.ApplicationConstant;
 
 /**
@@ -26,21 +29,38 @@ public class FastJsonConfiguration {
   }
 
   @Bean
-  public HttpMessageConverters fastjsonHttpMessageConverter() {
+  public HttpMessageConverters fastJsonHttpMessageConverter() {
     //定义一个转换消息的对象
     FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
 
-    //添加fastjson的配置信息 比如 ：是否要格式化返回的json数据
-    FastJsonConfig fastJsonConfig = new FastJsonConfig();
+    List<MediaType> supportedMediaTypes = new ArrayList<>();
+    supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+    supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+    supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+    supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+    supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+    supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+    supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+    supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+    supportedMediaTypes.add(MediaType.APPLICATION_XML);
+    supportedMediaTypes.add(MediaType.IMAGE_GIF);
+    supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+    supportedMediaTypes.add(MediaType.IMAGE_PNG);
+    supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+    supportedMediaTypes.add(MediaType.TEXT_HTML);
+    supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+    supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+    supportedMediaTypes.add(MediaType.TEXT_XML);
+    fastConverter.setSupportedMediaTypes(supportedMediaTypes);
 
-    fastJsonConfig
-        .setSerializerFeatures(SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue);
+    //添加fastJson的配置信息
+    FastJsonConfig fastJsonConfig = new FastJsonConfig();
+    fastJsonConfig.setSerializerFeatures(
+        SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue);
 
     //在转换器中添加配置信息
     fastConverter.setFastJsonConfig(fastJsonConfig);
-
     return new HttpMessageConverters(fastConverter);
-
   }
 
 }
