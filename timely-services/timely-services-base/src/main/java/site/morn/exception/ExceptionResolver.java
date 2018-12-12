a9bb224ed3f68590ea2f4bb120bbefb81c29af12
timely-rain <lolic_sd@hotmail.com>
2018-12-13 00:17:44 +0800

    异常移交给ExceptionHandlerAspect处理

--- timely-services/timely-services-base/src/main/java/site/morn/exception/ExceptionResolver.java
+++ timely-services/timely-services-base/src/main/java/site/morn/exception/ExceptionResolver.java
@@ -1,18 +1,15 @@
 package site.morn.exception;
 
-import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import site.morn.rest.RestMessage;
 import site.morn.rest.Rests;
-import site.morn.util.HttpUtils.ContentType;
-import site.morn.util.HttpUtils.Header;
 
 /**
  * Controller异常处理
@@ -25,25 +22,19 @@ import site.morn.util.HttpUtils.Header;
 @ControllerAdvice
 public class ExceptionResolver {
 
-  private final ExceptionProcessor exceptionProcessor;
-
-  @Autowired
-  public ExceptionResolver(ExceptionProcessor exceptionProcessor) {
-    this.exceptionProcessor = exceptionProcessor;
-  }
-
+  /**
+   * 解析异常
+   *
+   * @param request 请求
+   * @param response 响应
+   * @param e 异常
+   * @return REST模型
+   */
   @ExceptionHandler
   @ResponseBody
-  public RestMessage doResolveException(HttpServletRequest request, HttpServletResponse response,
+  public RestMessage resolveException(HttpServletRequest request, HttpServletResponse response,
       Exception e) {
-    response.setHeader(Header.CONTENT_TYPE, ContentType.JSON);
-    ExceptionMessage exceptionMessage = exceptionProcessor.process(e);
-    if (Objects.isNull(exceptionMessage)) {
-      return Rests.error();
-    }
-    if (StringUtils.isEmpty(exceptionMessage.getCode())) {
-      exceptionMessage.setCode(Rests.CODE_ERROR);
-    }
-    return Rests.error().code(exceptionMessage.getCode()).message(exceptionMessage.getMessage());
+    response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
+    return Rests.error().message(e.getMessage());
   }
 }
