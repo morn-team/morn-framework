c5b192dd0685f8054a2be37d785c416a9ef381ed
timely-rain <lolic_sd@hotmail.com>
2019-01-09 22:08:53 +0800

    移除无效代码

--- morn-services/morn-services-user/src/main/java/site/morn/services/base/repository/PrivilegeRepository.java
+++ morn-services/morn-services-user/src/main/java/site/morn/services/base/repository/PrivilegeRepository.java
@@ -1,6 +1,6 @@
 package site.morn.services.base.repository;
 
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import site.morn.services.base.domain.Privilege;
 
 /**
@@ -10,15 +10,7 @@ import site.morn.services.base.domain.Privilege;
  * @version 1.0.0, 2017/9/18
  * @since 1.0-SNAPSHOT
  */
-//@NoRepositoryBean
-@RepositoryRestResource
+@Repository
 public interface PrivilegeRepository extends BaseRepository<Privilege, Long> {
-//    /**
-//     * 所有权限码
-//     *
-//     * @param username 用户名
-//     * @return 权限码集合
-//     */
-//    @Query("select code from Privilege where roles.users.username = :username")
-//    List<String> findCodes(@Param("username") String username);
+
 }
