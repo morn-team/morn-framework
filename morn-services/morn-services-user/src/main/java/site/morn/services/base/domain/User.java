71f2b779ebbd325276ba30218bc8f8c5c78e86cb
timely-rain <lolic_sd@hotmail.com>
2019-01-16 21:47:23 +0800

    refactor:使用Lombok简化代码

--- morn-services/morn-services-user/src/main/java/site/morn/services/base/domain/User.java
+++ morn-services/morn-services-user/src/main/java/site/morn/services/base/domain/User.java
@@ -13,23 +13,23 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
 
 /**
  * 用户
  *
- * @author TianGanLin
- * @version 1.0.0, 2017/9/10
- * @since 1.0-SNAPSHOT
+ * @author timely-rain
+ * @since 1.0.0, 2017/9/10
  */
+@Data
 @Entity
 @Inheritance
 @DynamicInsert
 @DynamicUpdate
 public class User implements java.io.Serializable {
-
-  // Fields
 
   /**
    * 主键
@@ -55,17 +55,12 @@ public class User implements java.io.Serializable {
    * 昵称
    */
   @Column(length = 32)
-  private String nickname;        // 昵称
-  /**
-   * 真实姓名
-   */
-  @Column(length = 32)
-  private String realname;        // 真实姓名
+  private String nickname;        // 姓名
   /**
    * 身份证
    */
   @Column(length = 18)
-  private String idnumber;        // 身份证
+  private String idNumber;        // 身份证
   /**
    * 头像
    */
@@ -88,142 +83,21 @@ public class User implements java.io.Serializable {
   @Temporal(TemporalType.DATE)
   private Date birthday;          // 生日
   /**
-   * 录入时间
-   */
-  @Column(nullable = false)
-  @Temporal(TemporalType.DATE)
-  private Date date;              // 创建时间
-  /**
    * 手机号
    */
   @Column
   private String phone;           // 手机号
+  /**
+   * 录入时间
+   */
+  @CreatedDate
+  @Column
+  @Temporal(TemporalType.DATE)
+  private Date date;              // 创建时间
   /**
    * 角色
    */
   @ManyToMany
   @JoinColumn
   private Set<Role> roles;
-
-  // Constructors
-
-  /**
-   * default constructor
-   */
-  public User() {
-  }
-
-  /**
-   * minimal constructor
-   */
-  public User(String username) {
-    this();
-    this.username = username;
-  }
-
-  // Getter And Setter
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getNickname() {
-    return nickname;
-  }
-
-  public void setNickname(String nickname) {
-    this.nickname = nickname;
-  }
-
-  public String getRealname() {
-    return realname;
-  }
-
-  public void setRealname(String realname) {
-    this.realname = realname;
-  }
-
-  public String getIdnumber() {
-    return idnumber;
-  }
-
-  public void setIdnumber(String idnumber) {
-    this.idnumber = idnumber;
-  }
-
-  public String getIcon() {
-    return icon;
-  }
-
-  public void setIcon(String icon) {
-    this.icon = icon;
-  }
-
-  public String getIconSm() {
-    return iconSm;
-  }
-
-  public void setIconSm(String iconSm) {
-    this.iconSm = iconSm;
-  }
-
-  public String getSex() {
-    return sex;
-  }
-
-  public void setSex(String sex) {
-    this.sex = sex;
-  }
-
-  public Date getBirthday() {
-    return birthday;
-  }
-
-  public void setBirthday(Date birthday) {
-    this.birthday = birthday;
-  }
-
-  public Date getDate() {
-    return date;
-  }
-
-  public void setDate(Date date) {
-    this.date = date;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
-  }
 }
