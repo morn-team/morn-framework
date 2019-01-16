a9af1666599166ad831413ec1f48acdc27969aad
timely-rain <lolic_sd@hotmail.com>
2019-01-16 23:47:50 +0800

    refactor:新增功能移入基础控制器

--- morn-services/morn-services-user/src/main/java/site/morn/services/base/controller/UserController.java
+++ morn-services/morn-services-user/src/main/java/site/morn/services/base/controller/UserController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import java.util.Map;
 import javax.annotation.Resource;
 import org.apache.shiro.SecurityUtils;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.morn.boot.support.CrudControllerSupport;
@@ -33,11 +30,6 @@ public class UserController extends CrudControllerSupport<User, Long, UserServic
    */
   @Resource
   private PrivilegeService privilegeService;
-
-  @PostMapping
-  public RestMessage add(@Validated @RequestBody User user) {
-    return RestBuilders.successMessage();
-  }
 
   /**
    * 获取用户信息
