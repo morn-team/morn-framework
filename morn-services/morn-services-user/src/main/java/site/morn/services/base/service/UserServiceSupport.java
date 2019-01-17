2203a4869c7a62af66a9802551784b7711cbc519
timely-rain <lolic_sd@hotmail.com>
2019-01-17 22:48:35 +0800

    fix:移除无效代码

--- morn-services/morn-services-user/src/main/java/site/morn/services/base/service/UserServiceSupport.java
+++ morn-services/morn-services-user/src/main/java/site/morn/services/base/service/UserServiceSupport.java
@@ -1,7 +1,5 @@
 package site.morn.services.base.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,31 +28,6 @@ public class UserServiceSupport extends CrudServiceSupport<User, Long, UserRepos
   @Override
   public void deleteByUsername(String username) {
     repository().deleteByUsername(username);
-  }
-
-  @Override
-  public List<String> getPrivilegeCodes(User user) {
-
-    List<String> list = new ArrayList<>();
-//        if (ConstantUtils.isAdmin(user.getUsername()))
-//        {
-//            list = privilegeService.getAllCodes();
-//        }
-//        else
-//        {
-//            user = userDao.findOne(user.getId());
-//            Set<Role> roles = user.getRoles();
-//            for (Role role : roles)
-//            {
-//                Set<Privilege> privileges = role.getPrivileges();
-//                for (Privilege privilege : privileges)
-//                {
-//                    list.add(privilege.getPrivilegeCode());
-//                }
-//            }
-//        }
-
-    return list;
   }
 
   @Override
