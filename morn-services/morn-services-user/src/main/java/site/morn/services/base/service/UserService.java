24f6d1aeabd5c3db357c1a25318801ec6baa2eab
timely-rain <lolic_sd@hotmail.com>
2019-01-17 22:48:35 +0800

    fix:移除无效代码

--- morn-services/morn-services-user/src/main/java/site/morn/services/base/service/UserService.java
+++ morn-services/morn-services-user/src/main/java/site/morn/services/base/service/UserService.java
@@ -1,7 +1,6 @@
 package site.morn.services.base.service;
 
 
-import java.util.List;
 import site.morn.application.user.User;
 import site.morn.boot.support.CrudService;
 
@@ -16,13 +15,5 @@ public interface UserService extends CrudService<User, Long> {
   User findByUsername(String username);
 
   void deleteByUsername(String username);
-
-  /**
-   * 查询所有权限码
-   *
-   * @param user 用户
-   * @return 权限码集合
-   */
-  List<String> getPrivilegeCodes(User user);
 
 }
