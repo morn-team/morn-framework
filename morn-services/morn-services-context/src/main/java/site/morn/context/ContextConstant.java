7b66dd7f6b223ad23ed1722cf1a5e51894a8cd9e
timely-rain <lolic_sd@hotmail.com>
2019-01-17 21:52:30 +0800

    feat:当前用户相关功能

---
+++ morn-services/morn-services-context/src/main/java/site/morn/context/ContextConstant.java
@@ -0,0 +1,18 @@
+package site.morn.context;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * 上下文常量
+ *
+ * @author timely-rain
+ * @since 1.0.0, 2019/1/17
+ */
+@UtilityClass
+public class ContextConstant {
+
+  /**
+   * 管理员账号
+   */
+  public static final String ADMIN = "admin";
+}
