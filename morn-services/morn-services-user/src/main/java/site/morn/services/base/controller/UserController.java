450b69b32f6f0598dd48055b2639370e247d65f8
timely-rain <lolic_sd@hotmail.com>
2019-01-20 22:04:02 +0800

    feat:获取用户信息、搜索用户

--- morn-services/morn-services-user/src/main/java/site/morn/services/base/controller/UserController.java
+++ morn-services/morn-services-user/src/main/java/site/morn/services/base/controller/UserController.java
@@ -1,19 +1,25 @@
 package site.morn.services.base.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.annotation.Resource;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.morn.application.user.User;
 import site.morn.boot.support.CrudControllerSupport;
 import site.morn.context.ContextHolder;
+import site.morn.rest.RestBuilder;
 import site.morn.rest.RestBuilders;
 import site.morn.rest.RestMessage;
+import site.morn.rest.RestModel;
 import site.morn.services.base.service.PrivilegeService;
 import site.morn.services.base.service.UserService;
+import site.morn.services.user.UserConstant;
+import site.morn.validate.group.Put;
+import site.morn.validate.group.Update;
 
 /**
  * 用户控制器
@@ -32,17 +38,32 @@ public class UserController extends CrudControllerSupport<User, Long, UserServic
   private PrivilegeService privilegeService;
 
   /**
+   * 修改
+   */
+  @PutMapping
+  @Override
+  public RestMessage update(
+      @Validated({Update.class, Put.class}) @RequestBody RestModel<User> restModel) {
+    User user = service().update(restModel);
+    return RestBuilders.successMessage(user);
+  }
+
+  /**
    * 获取用户信息
    *
    * @return 用户信息
    */
   @GetMapping("info")
   public RestMessage info() {
+    // 当前登录用户
     User user = ContextHolder.currentUser();
+    // 权限码
     List<String> codes = privilegeService.findCodes(user);
-    Map<String, Object> data = new HashMap<>();
-    codes.add("admin");
-    data.put("privileges", codes);
-    return RestBuilders.successMessage(data);
+    codes.add("admin"); // 管理员权限
+    // 构建REST消息
+    RestBuilder builder = RestBuilders.successBuilder()
+        .data(UserConstant.INFO, user)
+        .data(UserConstant.PRIVILEGES, codes);
+    return builder.build();
   }
 }
