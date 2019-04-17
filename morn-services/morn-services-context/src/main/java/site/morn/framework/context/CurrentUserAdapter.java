0916c7f1a8f48e4264df8d80cdff992256707577
timely-rain <lolic_sd@hotmail.com>
2019-04-17 19:09:42 +0800

    chore:使用Sonatype仓库

--- morn-services/morn-services-context/src/main/java/site/morn/framework/context/CurrentUserAdapter.java
+++ morn-services/morn-services-context/src/main/java/site/morn/framework/context/CurrentUserAdapter.java
@@ -3,7 +3,7 @@ package site.morn.framework.context;
 /**
  * 当前用户适配器
  *
- * @author TianGanLin
+ * @author timely-rain
  * @since 0.0.1-SNAPSHOT, 2019/4/16
  */
 public interface CurrentUserAdapter<T> {
