12148778a540a613b636eb9ec78bfcc4ea253600
gltiana <gltiana@isoftstone.com>
2019-04-17 17:20:49 +0800

    style:格式化代码

--- morn-services/morn-services-base/src/main/java/site/morn/framework/exception/ExceptionResolver.java
+++ morn-services/morn-services-base/src/main/java/site/morn/framework/exception/ExceptionResolver.java
@@ -1,15 +1,12 @@
 package site.morn.framework.exception;
 
 import java.util.Optional;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import lombok.extern.slf4j.Slf4j;
 import site.morn.rest.RestBuilders;
 import site.morn.rest.RestMessage;
 
