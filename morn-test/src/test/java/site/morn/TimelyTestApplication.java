8a997d520298e9f49775abee91329ef5984169bd
timely-rain <lolic_sd@hotmail.com>
2019-04-24 23:52:01 +0800

    chore:单元测试
    用户单元测试

--- morn-test/src/test/java/site/morn/TimelyTestApplication.java
+++ morn-test/src/test/java/site/morn/TimelyTestApplication.java
@@ -2,9 +2,12 @@ package site.morn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import site.morn.boot.support.JpaRepositoryFactoryBean;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = JpaRepositoryFactoryBean.class)
 @EnableTransactionManagement
 public class TimelyTestApplication {
 
