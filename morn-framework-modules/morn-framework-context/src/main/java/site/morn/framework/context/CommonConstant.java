dae9afeef5c4b6b198d54bbf946f955be3a5391c
timely-rain <lolic_sd@hotmail.com>
2019-05-21 15:31:03 +0800

    feat:账户上下文
    - 获取当前机构

--- morn-framework-modules/morn-framework-context/src/main/java/site/morn/framework/context/CommonConstant.java
+++ morn-framework-modules/morn-framework-context/src/main/java/site/morn/framework/context/CommonConstant.java
@@ -15,4 +15,16 @@ public class CommonConstant {
    * 管理员账号
    */
   public static final String ADMIN = "admin";
+
+  /**
+   * 上下文常量
+   */
+  @UtilityClass
+  public class Context {
+
+    /**
+     * 激活的组织机构
+     */
+    public static final String ACTIVE_DEPARTMENT = "activeDepartment";
+  }
 }
