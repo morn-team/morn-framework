18be23326d5598d93ae51b5505c2e720a6721aa4
timely-rain <lolic_sd@hotmail.com>
2019-05-23 17:53:12 +0800

    feat:账号上下文
    - 判断用户是否为管理员
    - 用户取消屏蔽密码
    - 更新常量

--- morn-framework-modules/morn-framework-context/src/main/java/site/morn/framework/context/CommonConstant.java
+++ morn-framework-modules/morn-framework-context/src/main/java/site/morn/framework/context/CommonConstant.java
@@ -27,4 +27,48 @@ public class CommonConstant {
      */
     public static final String ACTIVE_DEPARTMENT = "activeDepartment";
   }
+
+  /**
+   * 错误码
+   */
+  @UtilityClass
+  public class Errors {
+
+    /**
+     * 新增失败
+     */
+    public static final String ADD_FAILURE = "common.add-failure";
+
+    /**
+     * 数据重复
+     */
+    public static final String PARAMS_DUPLICATION = "common.params-duplication";
+  }
+
+  /**
+   * 数值/字符串值
+   */
+  @UtilityClass
+  public class Values {
+
+    /**
+     * 假
+     */
+    public static final int FALSE_VALUE = 0;
+
+    /**
+     * 假
+     */
+    public static final String FALSE_STRING = "false";
+
+    /**
+     * 真
+     */
+    public static final int TRUE_VALUE = 1;
+
+    /**
+     * 真
+     */
+    public static final String TRUE_STRING = "true";
+  }
 }
